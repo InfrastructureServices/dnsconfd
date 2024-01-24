7bc0573de186fcda0c1fb236cb7ad5af3a8fb12a
Tomas Korbar <tkorbar@redhat.com>
2024-01-24 14:32:09 +0100

    Fix LICENSE file

--- tests/build_package.sh
+++ tests/build_package.sh
@@ -3,7 +3,6 @@
 set -e
 
 python3 setup.py sdist -d ./distribution
-cp LICENSE ./distribution/
 pushd distribution
 fedpkg --release=f39 mockbuild
 mv ./results_dnsconfd/0.0.1/1.fc39/*.noarch.rpm ../tests
