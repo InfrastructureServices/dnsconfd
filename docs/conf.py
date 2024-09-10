d3ea741cc1f21a97bdf5b67c95c12d936c857183
Tomas Korbar <tkorbar@redhat.com>
2024-09-10 15:23:46 +0200

    Update manpages

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tomas Korbar, Petr Mensik'
 author = 'Tomas Korbar, Petr Mensik'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.3.1'
 
 
 # -- General network_objects ---------------------------------------------------
