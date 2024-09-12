3a068d2eaf790108ed01cee3ccc70c2f9b330310
Tomas Korbar <tkorbar@redhat.com>
2024-09-12 15:18:12 +0200

    Improve Unbound manager unit tests

--- unittests/dns_managers/test_unbound_manager.py
+++ unittests/dns_managers/test_unbound_manager.py
@@ -5,8 +5,7 @@ import pytest
 import socket
 
 
-@pytest.fixture(scope="module")
-def instance():
+def create_instance():
     ins = UnboundManager()
     ins.lgr.disabled = True
     setattr(ins, "_executed_commands", [])
@@ -17,6 +16,11 @@ def instance():
 
     ins._execute_cmd = save_command
     return ins
+
+
+@pytest.fixture(scope="module")
+def instance_one():
+    return create_instance()
 
 
 @pytest.mark.parametrize("zones_to_servers, commands", [
@@ -41,14 +45,27 @@ def instance():
                               53,
                               "dummy.com")]},
      ["forward_remove dummy.com",
-      "flush_zone dummy.com"])
+      "flush_zone dummy.com"]),
+    ({},
+     ["forward_remove .",
+      "flush_zone ."]),
+    ({"dummy.com": [ServerDescription(socket.AF_INET,
+                                      bytes([192, 168, 9, 3]),
+                                      None,
+                                      "dummy.com",
+                                      domains=[("dummy.com", True)],
+                                      protocol=DnsProtocol.DNS_OVER_TLS)]},
+     ["flush_zone dummy.com",
+      "forward_add +t dummy.com 192.168.9.3@853#dummy.com"]),
+    ({"dummy.com": [ServerDescription(socket.AF_INET,
+                                      bytes([192, 168, 9, 3]),
+                                      None,
+                                      domains=[("dummy.com", True)])]},
+     ["forward_remove dummy.com",
+      "flush_zone dummy.com",
+      "forward_add dummy.com 192.168.9.3"])
 ])
-def test_first_update(zones_to_servers, commands, instance):
-    getattr(instance, "_executed_commands").clear()
-    instance.update({".": [ServerDescription(socket.AF_INET,
-                                             bytes([192, 168, 9, 3]),
-                                             53,
-                                             "dummy.com")]
-                     })
-    instance.update(zones_to_servers)
-    assert getattr(instance, "_executed_commands") == commands
+def test_first_update(zones_to_servers, commands, instance_one):
+    getattr(instance_one, "_executed_commands").clear()
+    instance_one.update(zones_to_servers)
+    assert getattr(instance_one, "_executed_commands") == commands
