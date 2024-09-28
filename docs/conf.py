1f7ff91aa24aabe8910500fa0481cb944b2c7a52
Tomas Korbar <tkorbar@redhat.com>
2024-09-28 17:01:58 +0200

    Change release to 1.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tomas Korbar, Petr Mensik'
 author = 'Tomas Korbar, Petr Mensik'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.1'
+release = '1.4.0'
 
 
 # -- General network_objects ---------------------------------------------------
