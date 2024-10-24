1ea27c81110ac1002655ebc6083904f08a8968c4
Tomas Korbar <tkorbar@redhat.com>
2024-10-24 16:32:23 +0200

    Add logging to file, journald and syslog
    New options are now available: stderr-log, journal-log,
    syslog-log, file-log
    
    By default service now logs through journal and has enhanced
    formatting showing priority.

--- dnsconfd/configuration/__init__.py
+++ dnsconfd/configuration/__init__.py
@@ -7,4 +7,5 @@ from .string_option import StringOption
 from .ip_option import IpOption
 from .static_servers_option import StaticServersOption
 from .bool_option import BoolOption
+from .syslog_option import SyslogOption
 from .dnsconfd_argument_parser import DnsconfdArgumentParser
