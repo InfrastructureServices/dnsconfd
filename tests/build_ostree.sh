096cf12d98be811bcb1aa41c1f7a5ad7368bc08b
Vladimír Beneš <vbenes@redhat.com>
2025-02-19 22:32:07 +0100

    tests: update NM just to version 1.51.90 from copr
    Newer NetworkManager packages have all fixes needed for dnsconf.

--- tests/build_ostree.sh
+++ tests/build_ostree.sh
@@ -8,17 +8,17 @@ fi;
 if grep el10 /etc/os-release; then
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable tkorbar/unbound-latest-centos centos-stream-10
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install -y unbound-1.20.0-9*
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable bengal/NetworkManager-dns centos-stream-10
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install -y NetworkManager-1.53.0*
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable networkmanager/NetworkManager-1.52-debug centos-stream-10
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" upgrade -y NetworkManager-1.51.90*
 elif grep el9 /etc/os-release; then
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable tkorbar/unbound-latest-centos centos-stream-9
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install -y unbound-1.16.2-17*
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable bengal/NetworkManager-dns centos-stream-9
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install -y NetworkManager-1.53.0*
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable networkmanager/NetworkManager-1.52-debug centos-stream-9
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" upgrade -y NetworkManager-1.51.90*
 elif grep f40 /etc/os-release; then
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable bengal/NetworkManager-dns
-  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install -y NetworkManager-1.53.0*
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" copr enable networkmanager/NetworkManager-1.52-debug
+  dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" upgrade -y NetworkManager-1.51.90*
 else
-  dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" copr enable bengal/NetworkManager-dns
-  dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" install -y NetworkManager-1.53.0*
+  dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" copr enable networkmanager/NetworkManager-1.52-debug 
+  dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" upgrade -y NetworkManager-1.51.90*
 fi
