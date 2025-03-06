c9b6d9bb26969f2b1f6cc0ace4a8bce45e8f9d30
Tomas Korbar <tkorbar@redhat.com>
2025-03-06 11:19:52 +0100

    Fix build_ostree test

--- tests/build_ostree.sh
+++ tests/build_ostree.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 # NOTE: we have to install openvpn here, because on CentosStream it is in EPEL and container
 # build does not have epel repository
 if grep el10 /etc/os-release || grep el9 /etc/os-release; then
