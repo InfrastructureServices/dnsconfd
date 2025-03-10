69ec1a3a10b2db97cc94123efa050399073b5cb6
Tomas Korbar <tkorbar@redhat.com>
2025-03-10 13:15:57 +0100

    Fix plans execution in RHEL environment

--- tests/build_ostree.sh
+++ tests/build_ostree.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -e
-# NOTE: we have to install openvpn here, because on CentosStream it is in EPEL and container
-# build does not have epel repository
 if grep el10 /etc/os-release || grep el9 /etc/os-release; then
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install dnf system-release openvpn NetworkManager-openvpn selinux-policy selinux-policy-base
+  cp -r /etc/yum.repos.d "$PWD/tests/baseroot/etc"
 else
   dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" install dnf system-release openvpn NetworkManager-openvpn selinux-policy selinux-policy-base
 fi;
