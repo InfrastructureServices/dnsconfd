49ef67fb1fd0802263dea2248e7a2b3f68c9e30b
petr sklenar <psklenar@gmail.com>
2025-06-04 13:40:58 +0200

    Update build_ostree.sh
    --redhat-brew-build needs extra packages

--- tests/build_ostree.sh
+++ tests/build_ostree.sh
@@ -4,6 +4,9 @@ set -e
 if grep el10 /etc/os-release || grep el9 /etc/os-release; then
   dnf -y --setopt=install_weak_deps=False --releasever=/ --installroot="$PWD/tests/baseroot" install dnf system-release openvpn NetworkManager-openvpn selinux-policy selinux-policy-base
   cp -r /etc/yum.repos.d "$PWD/tests/baseroot/etc"
+  # When installing from other source, option --brew-build is used
+  mkdir -p "$PWD/tests/baseroot/var/share"
+  cp -r /var/share/test-artifacts "$PWD/tests/baseroot/var/share"
 else
   dnf -y --setopt=install_weak_deps=False --use-host-config --installroot="$PWD/tests/baseroot" install dnf system-release openvpn NetworkManager-openvpn selinux-policy selinux-policy-base
 fi;
