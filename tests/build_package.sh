bf9b63d1e7dd8796fab8c97b6c56e4a9528daac4
Tomas Korbar <tkorbar@redhat.com>
2026-02-16 10:49:04 +0100

    Fix multihost plan

--- tests/build_package.sh
+++ tests/build_package.sh
@@ -3,7 +3,7 @@
 set -e
 
 tempdir=$(mktemp -d)
-tar -czvf $tempdir/dnsconfd-2.0.0.tar.gz .
+tar -czvf $tempdir/dnsconfd-2.0.0.tar.gz --transform 's,^\./,dnsconfd-2.0.0/,' .
 mkdir SOURCES
 cp ./distribution/dnsconfd.sysusers $tempdir/dnsconfd-2.0.0.tar.gz ./SOURCES
 # there is a hidden side effect of bb and that is that it copies sources from
