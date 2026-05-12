0dff2126fb3420ec4f55b35f8c165f8196e3b50f
Tomas Korbar <tkorbar@redhat.com>
2026-05-12 14:08:17 +0200

    Preventively quote tempdir variable in build_package script

--- tests/build_package.sh
+++ tests/build_package.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-tempdir=$(mktemp -d)
-tar -czvf $tempdir/dnsconfd-2.2.0.tar.gz --transform 's,^\./,dnsconfd-2.2.0/,' .
+tempdir="$(mktemp -d)"
+tar -czvf "$tempdir/dnsconfd-2.2.0.tar.gz" --transform 's,^\./,dnsconfd-2.2.0/,' .
 mkdir SOURCES
-cp ./distribution/dnsconfd.sysusers $tempdir/dnsconfd-2.2.0.tar.gz ./SOURCES
+cp ./distribution/dnsconfd.sysusers "$tempdir/dnsconfd-2.2.0.tar.gz" ./SOURCES
 # there is a hidden side effect of bb and that is that it copies sources from
 # SOURCES directory into binary rpms, however if they are missing, they
 # are not copied and scriptlets are left without them
 rpmbuild --define "_topdir $PWD" --define "with_asan 1" -bb distribution/dnsconfd.spec
 find ./RPMS -name "*.rpm" -exec cp "{}" ./tests \;
 rm -rf BUILDROOT RPMS SRPMS SOURCES
-rm -rf $tempdir
+rm -rf "$tempdir"
